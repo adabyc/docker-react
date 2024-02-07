85a275e77e2c895ad334cdbd23526adf9355b0fc
adabyc <adambyczek.dev@gmail.com>
2024-02-07 16:55:52 +0000

    travis file and react app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-	  Adams change !!! 2
+	  Adams change !!! 4.5
         </p>
         <a
           className="App-link"
